c29456485dac1896853bc51b60bf5c71a7c633cf
Robin Andersson <113249813+og-prips@users.noreply.github.com>
2023-09-19 15:22:37 +0200

    Initial commit for client and server

---
+++ Assignment1/Client/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Skapa en klient och anslut till servern
+            TcpClient client = new TcpClient();
+            int port = 1024; // Använd samma portnummer som servern
+            string serverIp = "127.0.0.1"; // Ersätt med serverns IP-adress
+
+            client.Connect(serverIp, port);
+            Console.WriteLine("Connected to the server.");
+
+            // Skicka data till servern
+            Console.WriteLine("Send a message to the server:");
+
+            string message = Console.ReadLine();
+            byte[] messageBytes = Encoding.ASCII.GetBytes(message);
+            NetworkStream stream = client.GetStream();
+            stream.Write(messageBytes, 0, messageBytes.Length);
+            Console.WriteLine("Message sent to server: " + message);
+
+            // Ta emot svar från servern
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string serverResponse = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Server response: " + serverResponse);
+
+            Console.ReadLine();
+
+            // Stäng klienten
+
+        }
+    }
+}
---
+++ Assignment1/Server/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 1024;
+            TcpListener listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+
+            Console.WriteLine("Server is up and running, listening on port: " + port);
+
+            // Vänta på klientanslutningar
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine("Client connected.");
+
+            // Skapa en tråd för att hantera kommunikation med klienten
+            Thread clientThread = new Thread(HandleClient);
+            clientThread.Start(client);
+
+            Console.ReadLine();
+        }
+
+        // Metod för att hantera kommunikation med klienten
+        static void HandleClient(object clientObj)
+        {
+            TcpClient client = (TcpClient)clientObj;
+
+            // Hämta klientens ström
+            NetworkStream stream = client.GetStream();
+
+            // Läs data från klienten
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string clientData = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Client sent: " + clientData);
+
+            // Skicka svar till klienten
+            string response = "Hello from the server!";
+            byte[] responseData = Encoding.ASCII.GetBytes(response);
+            stream.Write(responseData, 0, responseData.Length);
+
+            // Stäng anslutningen till klienten
+        }
+    }
+}
