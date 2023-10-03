0bb0236dd0cda8a8823754a7e353b269bbbe7d53
Robin Andersson <113249813+og-prips@users.noreply.github.com>
2023-10-03 09:57:17 +0200

    Add Message DTO

---
+++ Assignment3/Assignment3.Common/DTOs/Message.cs
@@ -0,0 +1,14 @@
+﻿namespace Assignment3.Common.DTOs
+{
+    public class Message
+    {
+        public string Text { get; set; }
+        public DateTime DateSent { get; set; }
+
+        public Message(string text, DateTime dateSent)
+        {
+            Text = text;
+            DateSent = dateSent;
+        }
+    }
+}
